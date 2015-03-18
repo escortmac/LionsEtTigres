48235becbd42b6f27211086dcfef059e13de22fd
PHILIPPE PESCATORE 2 <escortmac@pc8.home>
2015-03-18 12:42:56 +0100

    Révision 002

--- LionsEtTigres/Tigre.swift
+++ LionsEtTigres/Tigre.swift
@@ -15,4 +15,42 @@ struct Tigre {
     var race = ""
     var image = UIImage(named: "")
     
+    func crier() {
+        println("Tigre : rrrrr rrrrr")
+    }
+    func crierPlusieursFois (fois : Int) {
+        for var crier = 0 ; crier < fois ; ++crier {
+            self.crier()
+        }
+    }
+    
+    func crierPlusieursFois (fois : Int, estFort : Bool) {
+        for var crierRepetitions = 1; crierRepetitions <= fois; crierRepetitions++ {
+            if estFort {
+                crier()
+            } else {
+                println("Tigre: prout prout")
+            }
+        }
+    }
+    
+    func ageEnAnneesDeTigre (ageNormal: Int) -> Int {
+        let ageConverti = ageNormal * 3
+        return ageConverti
+    }
+    
+    func faitAleatoire () -> String {
+        let nombreAleatoire = Int(arc4random_uniform(UInt32(3)))
+        var faitAleatoire:String
+        
+        if nombreAleatoire == 0 {
+            faitAleatoire = "Le Tigre est la plus grosse espèce de félins"
+        } else if nombreAleatoire == 1 {
+            faitAleatoire = "Les tigres peuvent atteindre une longueur de 3,3 mètres"
+        } else {
+            faitAleatoire = "Un groupe de tigres s'appelle une Meute"
+        }
+        return faitAleatoire
+    }
+    
 }
--- LionsEtTigres/ViewController.swift
+++ LionsEtTigres/ViewController.swift
@@ -14,8 +14,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var nom: UILabel!
     @IBOutlet weak var age: UILabel!
     @IBOutlet weak var race: UILabel!
+    @IBOutlet weak var faitAleatoireEtiquette: UILabel!
     
-    var tigres:[Tigre] = []
+    var tigres:[Tigre] = [] // déclaration et init du tableau des Instances de la Classe Tigre
+    
+    var indiceCourant = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,6 +30,11 @@ class ViewController: UIViewController {
         unTigre.age = 5
         unTigre.image = UIImage (named: "BengalTiger.jpg")
         
+        unTigre.age = unTigre.ageEnAnneesDeTigre(unTigre.age)
+        
+        unTigre.crier()
+        unTigre.crierPlusieursFois(5, estFort: false)
+        
         tigres.append(unTigre)
         
         
@@ -36,6 +44,7 @@ class ViewController: UIViewController {
         nom.text = unTigre.nom
         age.text = "\(unTigre.age)"
         race.text = unTigre.race
+        faitAleatoireEtiquette.text = unTigre.faitAleatoire()
         
         var secondTigre = Tigre()
         secondTigre.nom = "Tigresse"
@@ -43,11 +52,17 @@ class ViewController: UIViewController {
         secondTigre.age = 2
         secondTigre.image = UIImage(named: "IndochineseTiger.jpg")
         
+        secondTigre.age = secondTigre.ageEnAnneesDeTigre(secondTigre.age)
+        
+        secondTigre.crier()
+        
         var troisiemeTigre = Tigre()
         troisiemeTigre.nom = "Jacob"
         troisiemeTigre.race = "Tigre malais"
         troisiemeTigre.age = 4
         troisiemeTigre.image = UIImage (named: "MalayanTiger.jpg")
+        
+        troisiemeTigre.age = troisiemeTigre.ageEnAnneesDeTigre(troisiemeTigre.age)
         
         var quatriemeTigre = Tigre()
         quatriemeTigre.nom = "Ynsigo"
@@ -55,7 +70,12 @@ class ViewController: UIViewController {
         quatriemeTigre.age = 6
         quatriemeTigre.image = UIImage (named: "SiberianTiger.jpg")
         
+        quatriemeTigre.age = quatriemeTigre.ageEnAnneesDeTigre(quatriemeTigre.age)
+        
         tigres += [secondTigre, troisiemeTigre, quatriemeTigre]
+        
+        unTigre.crierPlusieursFois(4)
+        secondTigre.crierPlusieursFois(2)
         
         
     }
@@ -67,12 +87,35 @@ class ViewController: UIViewController {
 
     @IBAction func boutonSuivantBarreDOutil(sender: UIBarButtonItem) {
         
-       let indiceAleatoire = Int(arc4random_uniform(UInt32(tigres.count)))
+        var indiceAleatoire:Int
+        
+        do {
+            indiceAleatoire = Int(arc4random_uniform(UInt32(tigres.count)))
+        } while indiceCourant == indiceAleatoire
+        
+        indiceCourant = indiceAleatoire
+        
         let tigre = tigres[indiceAleatoire]
-        monImage.image = tigre.image
-        nom.text = tigre.nom
-        age.text = "\(tigre.age)"
-        race.text = tigre.race
+//        monImage.image = tigre.image
+//        nom.text = tigre.nom
+//        age.text = "\(tigre.age)"
+//        race.text = tigre.race
+        
+        
+        UIView.transitionWithView(
+            self.view,
+            duration: 2,
+            options: UIViewAnimationOptions.TransitionCrossDissolve,
+            animations: {
+                self.monImage.image = tigre.image
+                self.nom.text = tigre.nom
+                self.age.text = "\(tigre.age)"
+                self.race.text = tigre.race
+                self.faitAleatoireEtiquette.text = tigre.faitAleatoire()
+            },
+            completion: {
+                (finished:Bool) -> () in
+        })
     }
 
 }
